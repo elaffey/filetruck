cf422ac6abe7c364c28c0208ac1f59a46b72af8e
Eamonn Laffey <eamonn.laffey@gmail.com>
2020-08-10 15:45:19 +1000

    More cleanup

--- src/commands.rs
+++ src/commands.rs
@@ -1,14 +1,8 @@
 use std::env;
 use std::path::{Path, PathBuf};
 
-use super::plan::Plan;
 use super::error::Error;
-
-fn append<P: AsRef<Path>>(path: &Path, to_append: P) -> PathBuf {
-    let mut path_buf = PathBuf::from(path);
-    path_buf.push(to_append);
-    path_buf
-}
+use super::plan::Plan;
 
 fn canonicalize(path: &Path) -> Result<PathBuf, Error> {
     path.canonicalize()
@@ -54,19 +48,19 @@ fn create_parent_dirs(path: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-fn copy_file_name(file: &str, input: &Path, output: &Path) -> Result<(), Error> {
-    let a = append(input, file);
-    is_file(&a)?;
-    let b = append(output, file);
-    create_parent_dirs(&b)?;
-    copy(&a, &b)?;
-    println!("{} -> {}", a.display(), b.display());
+fn copy_file_name(file: &str, mut from: PathBuf, mut to: PathBuf) -> Result<(), Error> {
+    from.push(file);
+    to.push(file);
+    is_file(&from)?;
+    create_parent_dirs(&to)?;
+    copy(&from, &to)?;
+    println!("{} -> {}", from.display(), to.display());
     Ok(())
 }
 
-fn copy_file_names(files: &Vec<String>, input: &Path, output: &Path) {
+fn copy_file_names(files: &Vec<String>, from: PathBuf, to: PathBuf) {
     for file in files {
-        match copy_file_name(file, input, output) {
+        match copy_file_name(file, from.clone(), to.clone()) {
             Ok(_) => {}
             Err(e) => {
                 eprintln!("{}", e);
@@ -80,16 +74,16 @@ fn current_dir() -> Result<PathBuf, Error> {
 }
 
 pub fn pick_up(plan: Plan, from: PathBuf) -> Result<(), Error> {
-    let cur_dir = current_dir()?;
-    let output = append(&cur_dir, &plan.name);
-    create_dir_all(&output)?;
-    copy_file_names(&plan.files, &from, &output);
+    let mut to = current_dir()?;
+    to.push(&plan.name);
+    create_dir_all(&to)?;
+    copy_file_names(&plan.files, from, to);
     Ok(())
 }
 
 pub fn drop_off(plan: Plan, to: PathBuf) -> Result<(), Error> {
-    let cur_dir = current_dir()?;
-    let input = append(&cur_dir, &plan.name);
-    copy_file_names(&plan.files, &input, &to);
+    let mut from = current_dir()?;
+    from.push(&plan.name);
+    copy_file_names(&plan.files, from, to);
     Ok(())
 }
